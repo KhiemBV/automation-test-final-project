5605ecb1f9913ab9d217b970fc4a3ccd46a45fbf
KhiemBV <khiema494@gmail.com>
2023-12-26 20:06:33 +0700

    update

--- src/main/java/automation/constant/ConstantAccount.java
+++ src/main/java/automation/constant/ConstantAccount.java
@@ -12,10 +12,5 @@ public class ConstantAccount {
 	public static By BUTTON_LOGOUT = By.xpath("//li[normalize-space()='Sign Out']");
 	public static By USER_AVATAR = By.id("user-dropdown");
 	public static By ADD_CLIENT_BOARD = By.id("ajaxModalContent");
-	
-	public static String ALADA_URL = "https://alada.vn/";
-	public static By ALADA_USER_AVATAR = By.xpath("//li[@id='showsub']");
 
-	public static String TRIZEN_URL = "https://techydevs.com/demos/themes/html/trizen-demo/trizen/index.html";
-	public static By SEARCH_RESULT = By.xpath("//h2[text()='Flight Search Result']");
 }
--- src/main/java/automation/testsuite/test.java
+++ src/main/java/automation/testsuite/test.java
@@ -1,5 +1,51 @@
 package automation.testsuite;
 
-public class test {
+import static org.testng.Assert.*;
 
+import org.testng.annotations.*;
+import automation.common.CommonBase;
+import automation.constant.ConstantAccount;
+import automation.page.Day13_LoginPage;
+
+public class test extends CommonBase{
+	
+	@Parameters("browser")
+	@BeforeMethod
+	public void openDriver(@Optional("chrome") String browser) {
+		driver = setupDriver(browser);
+		driver.get(ConstantAccount.RISE_URL);
+	}
+
+	@Test(priority = 1)
+	public void LoginSuccessfully() {
+		Day13_LoginPage login = new Day13_LoginPage(driver);
+		login.loginFuntion("admin@demo.com", "riseDemo");
+		assertEquals(true, driver.findElement(ConstantAccount.DASHBOARD_TEXT).isDisplayed());
+	}
+
+	@Test(priority = 2)
+	public void LoginFailIncorrertEmail() {
+		Day13_LoginPage login = new Day13_LoginPage(driver);
+		login.loginFuntion("ahihi@demo.com", "riseDemo");
+		assertEquals(true, driver.findElement(ConstantAccount.AUTHENICATION_ALERT).isDisplayed());
+	}
+	
+	@Test(priority = 3)
+	public void LoginFailIncorrertPassword() {
+		Day13_LoginPage login = new Day13_LoginPage(driver);
+		login.loginFuntion("admin@demo.com", "ahihi");
+		assertEquals(true, driver.findElement(ConstantAccount.AUTHENICATION_ALERT).isDisplayed());
+	}
+	
+	@Test(priority = 4)
+	public void LoginFailIncorrertEmail_Password() {
+		Day13_LoginPage login = new Day13_LoginPage(driver);
+		login.loginFuntion("ahihi@demo.com", "ahihi");
+		assertEquals(true, driver.findElement(ConstantAccount.AUTHENICATION_ALERT).isDisplayed());
+	}
+
+	@AfterMethod
+	public void closeChrome() {
+		closeDriver();
+	}
 }
